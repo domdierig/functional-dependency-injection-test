de53d8ec8f5def3a0c35e6b0d209ce5f84ffe054
Dominic Dierig <dierig@telemaxx.de>
2025-08-05 10:06:12 +0200

    remove stuff that is too much

--- src/dependency.ts
+++ src/dependency.ts
@@ -4,12 +4,4 @@ const writeToDBImpl:WriteToDBInterface = (data: string): void => {
     console.log(`Writing to DB: ${data}`);
 }
 
-const logToConsole = (message: string): void => {
-    console.log(`Log: ${message}`);
-}
-
-const sendMail = (recipient: string, subject: string, body: string): void => {
-    console.log(`Sending mail to ${recipient} with subject "${subject}" and body "${body}"`);
-}
-
-export { writeToDBImpl, logToConsole, sendMail };
+export { writeToDBImpl };
